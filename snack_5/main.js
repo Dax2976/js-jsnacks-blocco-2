5d93a851765166f179f5fb85ee81c5aab2d9932e
Dax2976 <ferrodavide55@gmail.com>
2022-03-31 15:33:12 +0200

    bonus fatto con for e while

---
+++ snack_5/main.js
@@ -0,0 +1,40 @@
+//Snack5 (Bonus)
+//Crea due array che hanno un numero di elementi diversi.
+//Aggiungi elementi all’array che ha meno elementi fino 
+//a quando ne avrà tanti quanti l’altro.
+
+
+
+let number = [1,2,3,4,5]
+
+let number2 = [1,2,3]
+
+
+for( i = 0; i < number.length; i++){
+    if(number2.length != number.length){
+        number2.push(Math.floor(Math.random()*100))
+    }
+}
+
+console.log(number)
+console.log(number2)
+
+
+
+//--------while situation---\\
+
+
+i = 0 
+
+while( i < number.length){
+    if(number2.length != number.length){
+        number2.push(Math.floor(Math.random()*100))
+    }
+
+    i++
+}
+
+
+console.log(number)
+console.log(number2)
+
